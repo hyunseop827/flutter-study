e541f29c73259bd4f160a7346312ea4332591281
Hyunseop Kim <gustjq2580@gmail.com>
2026-02-09 18:26:28 +0900

    [study] personality test - 09.02.20206

---
+++ personality_test/lib/main/mainlist_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPage();
+  }
+}
+
+class _MainPage extends State<MainPage> {
+
+  //json, asynchronous
+  Future<String> loadAsset() async {
+    return await rootBundle.loadString('res/api/list.json');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
